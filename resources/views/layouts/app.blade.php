5acd8de5c851841fa7c99604ad2283e3cc90e59d
Farhad Shikder <frd1952@gmail.com>
2020-11-10 00:09:32 +0600

    Note Layout setup

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<html lang="en">
+    <head>
+        <title>Todo App</title>
+        <link rel="stylesheet" href="{{asset('/vendor/bootstrap/css/bootstrap.min.css')}}">
+    </head>
+<body>
+    <!-- Navigation menu -->
+    <nav class="navbar bg-light">
+  <div class="container">
+<ul class="nav nav-pills w-100 justify-content-center">
+  <li class="nav-item mx-2">
+    <a class="nav-link {{request()->is("/") ? 'active' : ''}}" href="/">Notes</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link {{request()->is("/") ? 'active' : ''}}" href="/tags">Tags</a>
+  </li>
+</ul>
+</div>
+</nav>
+
+<!-- End naivation -->
+
+    @yield('main')
+</body>
+</html>
---
+++ resources/views/note/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('main')
+    <div class="container">
+        <div class="d-flex align-items-center justify-content-between mt-2">
+            <h5>Notes</h5>
+            <a href="/notes/create" class="btn btn-primary">Create</a>
+        </div>
+    </div>
+@endsection
