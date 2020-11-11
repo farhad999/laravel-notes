71500e6e4972a91cb65dedcc1393e16824479b46
Farhad Shikder <frd1952@gmail.com>
2020-11-11 22:51:34 +0600

    Update NoteImage.php

--- app/Models/NoteImage.php
+++ app/Models/NoteImage.php
@@ -5,7 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class NoteImages extends Model
+class NoteImage extends Model
 {
     use HasFactory;
+
+    function note(){
+        return $this->belongsTo(Note::class, 'note_id');
+    }
 }
